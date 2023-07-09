eb6d0ab045140acb2f9b99cb05c5bbdcb08d6f77
jmcarcell <jmcarcell@users.noreply.github.com>
2023-07-09 16:02:00 +0200

    Fix names

--- k4ProjectTemplate/src/components/ExampleConsumer.cpp
+++ k4ProjectTemplate/src/components/ExampleConsumer.cpp
@@ -9,13 +9,13 @@ using BaseClass_t = Gaudi::Functional::Traits::BaseClass_t<Gaudi::Algorithm>;
 struct ExampleConsumer final : Gaudi::Functional::Consumer<void( const int& ), BaseClass_t> {
 
   ExampleConsumer( const std::string& name, ISvcLocator* svcLoc )
-    : Consumer( name, svcLoc, KeyValue( "InputLocation", "/ExampleInt" ) ) {}
+    : Consumer( name, svcLoc, KeyValue( "ExampleConsumerInputLocation", "/ExampleInt" ) ) {}
 
   void operator()(const int& input) const override {
     info() << "ExampleInt = " << input << endmsg;
   }
 
-  Gaudi::Property<std::string> m_outputLocation{this, "InputLocation", "/ExampleInt",
+  Gaudi::Property<std::string> m_inputLocation{this, "ExampleConsumerInputLocation", "/ExampleInt",
                                                 "Location of the ExampleInt"};
 
 };
