53e9ebd65b9993f9f998778aaed75addd898281b
jmcarcell <jmcarcell@users.noreply.github.com>
2023-07-09 16:43:42 +0200

    Up

--- k4ProjectTemplate/src/components/ExampleProducer.cpp
+++ k4ProjectTemplate/src/components/ExampleProducer.cpp
@@ -9,13 +9,13 @@ using BaseClass_t = Gaudi::Functional::Traits::BaseClass_t<Gaudi::Algorithm>;
 struct ExampleProducer final : Gaudi::Functional::Producer<int(), BaseClass_t> {
 
   ExampleProducer( const std::string& name, ISvcLocator* svcLoc )
-    : Producer( name, svcLoc, KeyValue( "ExampleProducerOutputLocation", "/ExampleInt" ) ) {}
+    : Producer( name, svcLoc, KeyValue( "OutputLocation", "/ExampleInt" ) ) {}
 
   int operator()() const override {
     return m_exampleInt;
   }
 
-  Gaudi::Property<std::string> m_outputLocation{this, "ExampleProducerOutputLocation", "/ExampleInt",
+  Gaudi::Property<std::string> m_outputLocation{this, "OutputLocation", "/ExampleInt",
                                                 "Location of the ExampleInt"};
   Gaudi::Property<int> m_exampleInt{this, "ExampleInt", 3,
                                     "Example int to be produced"};
